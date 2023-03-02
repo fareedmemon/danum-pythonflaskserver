d5d1d1a8ab10469f5cff851a47aae32dd743c43e
Muhammad Fareed <43077684+fareedmemon@users.noreply.github.com>
2023-03-02 19:15:22 +0500

    fareed

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask("My Flask Application")
 def hello():
     return "<h1>My First Hello World!</h1>"
 
+
 if __name__=="__main__":
     app.run(debug=True) 
     # When no port is specified, starts at default port 5000
